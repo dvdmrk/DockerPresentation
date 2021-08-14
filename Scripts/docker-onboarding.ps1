f5054882a0bd3dfc17407a1c77b0d601c321cb29
davidmerk25@gmail.com <dvdmrk>
2021-08-14 11:29:36 -0400

    Updated onboarding script

--- Scripts/docker-onboarding.ps1
+++ Scripts/docker-onboarding.ps1
@@ -1,18 +1,20 @@
 # A simple setup script for windows machines
 
+# Required
+choco install docker-desktop -y --force
+
 # Technologies
-choco install nodejs.install -y --force 
+choco install nodejs -y --force # Not required - required on the container
 # choco install nvm -y --force # Recomended Alternative
 
 # Frameworks
-choco install dotnetcore-sdk -y --force
-npm install -g @angular/cli
+choco install dotnetcore-sdk -y --force # Not required - required on the container
 
 # Tools
-choco install visualstudiocode -y --force
-choco install docker-desktop -y --force
-choco install ssms -y --force
-choco install heidisql -y --force
+choco install visualstudiocode -y --force # Any IDE will work
+choco install ssms -y --force # For SQL
+choco install heidisql -y --force # For PostgreSQL
+choco install mysql.workbench -y --force # For MySQL
 
 
 
